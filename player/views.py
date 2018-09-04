157ec92d4917d321ed05354b52c245872a8ff178
Diego Campos <diecamdia@gmail.com>
2018-09-04 19:56:51 +0200

    Authentication

--- player/views.py
+++ player/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from gameplay.models import Game
 
-# Create your views here.
+@login_required
 def home(request):
     my_games = Game.objects.games_for_user(request.user)
     active_games = my_games.active()   
